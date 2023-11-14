2335fbeac884996af73470476c6d4d6c0ecf87fe
Xikaro <0regon.end@gmail.com>
2023-11-14 20:02:08 +0500

    Update Tools.groovy

--- groovy/postInit/Mod/GregTech/Tools.groovy
+++ groovy/postInit/Mod/GregTech/Tools.groovy
@@ -2,7 +2,7 @@
 
 if (isLoaded("gregtech")) {
 
-        def tongs_wood = item('gregtech:tongs').withNbt([DisallowContainerItem: (byte) 0, "GT.Tool": [Material: "wood", MaxDurability: 400, Durability: 0, AttackDamage: 5.0F, AttackSpeed: -2.4F], HideFlags: 2, "GT.Behaviours": []])
+        def tongs_wood = item('gregtech:tongs').withNbt([DisallowContainerItem: (byte) 0, "GT.Tool": [Material: "wood", MaxDurability: 70, Durability: 0, AttackDamage: 5.0F, AttackSpeed: -2.4F], HideFlags: 2, "GT.Behaviours": []])
         
         // --- Добавление рецептов
         
